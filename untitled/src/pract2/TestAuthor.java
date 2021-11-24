e57664214b7317c21a65821b3ffeeda3f7ba3555
Asphyxia777 <92206572+Asphyxia777@users.noreply.github.com>
2021-11-24 14:14:55 +0300

    Delete untitled directory

--- untitled/src/pract2/TestAuthor.java
+++
@@ -1,8 +0,0 @@
-package pract2;
-
-public class TestAuthor {
-    public static void main(String[] args) {
-        Author n = new Author ("Petr","petr@email.ru",'m');
-        System.out.println(n);
-    }
-}
